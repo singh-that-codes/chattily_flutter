2a1e3e25e27214a14e99c61fd755fc242ea65c05
singh-that-codes <500091028@stu.upes.ac.in>
2023-12-25 22:43:23 +0530

    Project started

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
+import 'package:chatify/my_app.dart';
 import 'package:flutter/material.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 final api_key="wur3mwmr996x";
-void main (){
+final user_token="nrea6gjvy68uzzq88zm29bgvpkqez8fvmkundt2z3qu68qndruks8pmdqughgmuh";
+void main ()async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //streamchat client
+  final client = StreamChatClient(
+    api_key,
+    logLevel:Level.INFO
+  );
+
+  //current user
+  await client.connectUser(User(id: "tutorial") ,user_token);
+
+  //get channel
+  final channel = client.channel("messaging",id:"flutterdevs");
+
+  await channel.watch();
+
+
   runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title:'Chatify',
-    );
-  }
 }
