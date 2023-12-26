c94e3eeb2ff50f2363ada4015724d0b239d2a5f5
singh-that-codes <500091028@stu.upes.ac.in>
2023-12-27 00:07:08 +0530

    Backend Functions written

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,44 @@
-import 'package:chatify/my_app.dart';
+import 'package:chatify/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:stream_chat_flutter/stream_chat_flutter.dart';
-final api_key="wur3mwmr996x";
-final user_token="nrea6gjvy68uzzq88zm29bgvpkqez8fvmkundt2z3qu68qndruks8pmdqughgmuh";
-void main ()async{
+import 'package:flutter/services.dart';
+Future<void> main()async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options:DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.white,
+    systemNavigationBarIconBrightness: Brightness.light,
+    statusBarColor: Colors.white,
+    statusBarIconBrightness: Brightness.dark,
+  )
+);
+}
 
-  //streamchat client
-  final client = StreamChatClient(
-    api_key,
-    logLevel:Level.INFO
-  );
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  //current user
-  await client.connectUser(User(id: "tutorial") ,user_token);
-
-  //get channel
-  final channel = client.channel("messaging",id:"flutterdevs");
-
-  await channel.watch();
-
-
-  runApp(MyApp());
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chattily',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white.withOpacity(0.5),
+        appBarTheme:AppBarTheme(
+          iconTheme:IconThemeData(
+            color: Colors.black.withOpacity(0.5),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        titleTextStyle: const TextStyle(
+          fontSize:18,
+          color:Colors.black,
+          fontWeight:FontWeight.w500,
+        )
+      ),
+      )
+    );
+  }
 }
---
+++ lib/utils.dart
@@ -0,0 +1,66 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+String currentUser = FirebaseAuth.instance.currentUser!.uid;
+
+CollectionReference usersCollection = FirebaseFirestore.instance.collection("users");
+
+CollectionReference chatsCollection({required String user}){
+  return usersCollection.doc(user).collection("chats");
+}
+
+CollectionReference messagesCollection({required String user, required String chat}){
+  return chatsCollection(user: user).doc(chat).collection('messages');
+}
+
+Stream<DocumentSnapshot> userData({required String id}) async* {
+  yield await usersCollection.doc(id).get();
+}
+
+Stream<List<DocumentSnapshot>> usersData() async* {
+  List <DocumentSnapshot> users = [];
+  await usersCollection.get().then(
+    (value) {
+      if(value.docs.isNotEmpty){
+        for(var element in value.docs){
+          users.add(element);
+        }
+      }
+    },
+  );
+}
+
+String formatChatDateTime(
+    {required DateTime dateTime, required BuildContext context}){
+  final now = DateTime.now();
+  bool is24HoursFormat=MediaQuery.of(context).alwaysUse24HourFormat;
+
+  String formattedTime = is24HoursFormat
+  ? DateFormat.Hm().format(dateTime)
+  : DateFormat('h:mm a').format(dateTime);
+
+
+  if (dateTime.year == now.year &&
+      dateTime.month==now.month &&
+      dateTime.day==now.day){
+        return formattedTime;
+      }
+  
+  final yesterday = now.subtract(const Duration(days:1));
+  if (dateTime.year == yesterday.year &&
+      dateTime.month == yesterday.month &&
+      dateTime.day == yesterday.day) {
+    return 'Yesterday, $formattedTime';
+  }
+
+  return DateFormat('MMM d').format(dateTime);
+}
+
+double deviceHeight({required BuildContext context}){
+  return MediaQuery.of(context).size.height;
+}
+double deviceWidth({required BuildContext context}) {
+  return MediaQuery.of(context).size.width;
+}
